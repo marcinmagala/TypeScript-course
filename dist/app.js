e66b5426a1d1921281e894c7e77a9f5ee006f479
marcinmagala <marcinmagala@gmail.com>
2022-09-29 01:42:33 +0200

    union types

--- dist/app.js
+++ dist/app.js
@@ -5,11 +5,17 @@
 // const add = (v1: number, v2: number) => v1 + v2;
 // console.log(add(1, 8));
 // Basic types in practice
-const input1 = document.querySelector("#input1");
-const input2 = document.querySelector("#input2");
-const addButtonElement = document.querySelector("button");
-const add = (v1, v2) => v1 + v2;
-addButtonElement.addEventListener("click", () => {
-    const sum = add(Number(input1.value), Number(input2.value));
-    console.log(sum);
-});
+// const input1: HTMLInputElement = document.querySelector("#input1");
+// const input2: HTMLInputElement = document.querySelector("#input2");
+// const addButtonElement = document.querySelector("button");
+// const add = (v1: number, v2: number) => v1 + v2;
+// addButtonElement.addEventListener("click", () => {
+//   const sum = add(Number(input1.value), Number(input2.value));
+//   console.log(sum);
+// });
+// Union types
+const logAge = (age) => {
+    console.log(`Mam ${age} lata.`);
+};
+logAge(24);
+logAge("dwadziescia cztery");
--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,22 @@
 
 // Basic types in practice
 
-const input1: HTMLInputElement = document.querySelector("#input1");
-const input2: HTMLInputElement = document.querySelector("#input2");
-const addButtonElement = document.querySelector("button");
+// const input1: HTMLInputElement = document.querySelector("#input1");
+// const input2: HTMLInputElement = document.querySelector("#input2");
+// const addButtonElement = document.querySelector("button");
 
-const add = (v1: number, v2: number) => v1 + v2;
+// const add = (v1: number, v2: number) => v1 + v2;
 
-addButtonElement.addEventListener("click", () => {
-  const sum = add(Number(input1.value), Number(input2.value));
-  console.log(sum);
-});
+// addButtonElement.addEventListener("click", () => {
+//   const sum = add(Number(input1.value), Number(input2.value));
+//   console.log(sum);
+// });
+
+// Union types
+
+const logAge = (age: number | string) => {
+  console.log(`Mam ${age} lata.`);
+};
+
+logAge(24);
+logAge("dwadziescia cztery");
