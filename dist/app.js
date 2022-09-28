028c2995bd2ee165eac9b26531ca7534e0f08f6b
marcinmagala <marcinmagala@gmail.com>
2022-09-29 01:26:19 +0200

    basic types in practice

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,15 @@
-let age = 24;
-age = 25;
-let ageAsString = "dwadziescia cztery";
-console.log(`${age}`);
+// let age: number = 24;
+// age = 25;
+// let ageAsString: string = "dwadziescia cztery";
+// console.log(`${age}`);
+// const add = (v1: number, v2: number) => v1 + v2;
+// console.log(add(1, 8));
+// Basic types in practice
+const input1 = document.querySelector("#input1");
+const input2 = document.querySelector("#input2");
+const addButtonElement = document.querySelector("button");
+const add = (v1, v2) => v1 + v2;
+addButtonElement.addEventListener("click", () => {
+    const sum = add(Number(input1.value), Number(input2.value));
+    console.log(sum);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
-let age: number = 24;
-age = 25;
+// let age: number = 24;
+// age = 25;
 
-let ageAsString: string = "dwadziescia cztery";
-console.log(`${age}`);
+// let ageAsString: string = "dwadziescia cztery";
+// console.log(`${age}`);
+
+// const add = (v1: number, v2: number) => v1 + v2;
+
+// console.log(add(1, 8));
+
+// Basic types in practice
+
+const input1: HTMLInputElement = document.querySelector("#input1");
+const input2: HTMLInputElement = document.querySelector("#input2");
+const addButtonElement = document.querySelector("button");
+
+const add = (v1: number, v2: number) => v1 + v2;
+
+addButtonElement.addEventListener("click", () => {
+  const sum = add(Number(input1.value), Number(input2.value));
+  console.log(sum);
+});
